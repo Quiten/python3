38f830c9d2e3dba8b87d16c45e9783827cfc7fe1
Quiten <37398178+Quiten@users.noreply.github.com>
2019-08-30 13:13:48 +0200

    Add files via upload

---
+++ functies.py
@@ -0,0 +1,32 @@
+pokemon_list = {"mimikyu, wooloo"}
+
+def test():
+    print("You have encountered a wild pokemon!")
+    print("A wild " + str(poke)  + " appeared")
+
+print("xxxxxxxxxx")
+
+print("test 1")
+
+def hello_world():
+    for i in range(10):
+        print("Hello world " + str(i + 1))
+
+def tafel_5():
+    for i in range(10):
+        print(str(5*(i+1)))
+
+print("test 2")
+
+def test(poke):
+    print("You have encountered a wild pokemon!")
+    print("A wild " + str(poke)  + " appeared")
+
+print("test 3")
+def names(naam):
+    print(naam + " :")
+    for i in range(len(naam)):
+        a = i + 1
+        print(str(naam[i])*a)
+
+names("mies")
