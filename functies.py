3151e5cdf65c16fa7b9f5fccbb41fd37221e9698
Quiten <quinten.de.leeuw@coderclass.nl>
2019-09-13 12:12:55 +0200

    JAWOHL
    Hoi ik ben een idot

--- functies.py
+++ functies.py
@@ -4,9 +4,9 @@ def test():
     print("You have encountered a wild pokemon!")
     print("A wild " + str(poke)  + " appeared")
 
-print("xxxxxxxxxx")
+#print("xxxxxxxxxx")
 
-print("test 1")
+#print("test 1")
 
 def hello_world():
     for i in range(10):
@@ -16,17 +16,78 @@ def tafel_5():
     for i in range(10):
         print(str(5*(i+1)))
 
-print("test 2")
+#print("test 2")
 
 def test(poke):
     print("You have encountered a wild pokemon!")
     print("A wild " + str(poke)  + " appeared")
 
-print("test 3")
+#print("test 3")
 def names(naam):
     print(naam + " :")
     for i in range(len(naam)):
         a = i + 1
         print(str(naam[i])*a)
 
-names("mies")
+#names("mies")
+
+#print("Test 4");
+
+def max_van_3(r, b, g):
+    if (r > b):
+        if (r > g):
+            return r
+    if (b > r):
+        if (b > g):
+            return b
+    if (g > r):
+        if (g > b):
+            return g
+
+#print(max_van_3(10, 11, 9))
+
+#print("Test 5")
+array = []
+def tester101(mijmij):
+    for i in range(len(mijmij)):
+        i += 1
+        p = i * -1
+        array.append(mijmij[p])
+        print(array)
+    return array
+#fullStr = ''.join(tester101("STEG"))
+#print(fullStr)
+
+#print("Test 6")
+
+def is_priemgetal(getal):
+        if (getal <= 3):
+            print(getal)
+            return "primus"
+        for i in range(2,getal):
+            if( (getal % i) == 0):
+                print("WHAAAA")
+                return "not optimus prime"
+            else:
+                return "Optimus Prime LOCATED"
+        else:
+            return "No Prime here"
+#print(is_priemgetal(5))
+
+print("Test 7")
+    array = []
+def tester404(mijmij):
+    for i in range(len(mijmij)):
+        i += 1
+        p = i * -1
+        array.append(mijmij[p])
+        print(array)
+    string = ''.join(array)
+    if (string == mijmij):
+        return array
+    else :
+        return "FALSE"
+#fullStr = ''.join(tester404("tacocat"))
+#print(fullStr)
+
+
---
+++ python_end.py
@@ -0,0 +1,141 @@
+i = 1
+opening = "true"
+
+def number_checker():
+    a = open("memes.txt", "r")
+    lines = a.readlines()
+    print(lines)
+    a.close()
+
+
+def nummer_erbij(getal):
+    return getal + 1
+
+def gevoel(hoe, waiter):
+    nummer = 1
+    #number_checker()
+    while(hoe != "esc"):
+        if (hoe == "toev"):
+            keuze = input("freestyle of locked ")
+            if (keuze == "locked"):
+                word = input("Woord ")
+                nummero = input("Nummer ")
+                with open("memes.txt", "a") as a:
+                    a.write("Woord : " + word + " , Nummer : " + nummero + "\n")
+                waiter = "false"
+                looper(hoe, waiter)
+                break
+            elif (keuze == "freestyle"):
+                freestyle_text = input("Wat moet er in de .txt bestand ")
+                print(freestyle_text)
+                with open("memes.txt", "a") as a:
+                    a.write(freestyle_text + "\n")
+                waiter = "false"
+                looper(hoe, waiter)
+                break
+            else:
+                print("error_writer")
+                waiter = "false"
+                looper(hoe, waiter)
+                break
+                    
+
+        elif (hoe == "read"):
+            with open("memes.txt", "r") as b:
+                line = b.readlines()
+                regel = input("Welke regel ? ; ")
+                if (regel != "alles"):
+                    regel2= int(regel)
+                    if (isinstance(regel2, int)):
+                        print(line[regel2-1])
+                    else:
+                        print("Failure")
+                    waiter = "false"
+                    looper(hoe, waiter)
+                    break
+                elif (regel == "alles"):
+                    for i, value in enumerate(line,1):
+                        print(i, value)
+                    waiter = "false"
+                    looper(hoe, waiter)
+                    break
+                else:
+                    print("dwadwaa")
+                    waiter = "false"
+                    looper(hoe, waiter)
+                    break
+        elif (hoe == "del"):
+            with open("memes.txt","r") as b:
+                line = b.readlines()
+            with open("memes.txt", "a") as b:
+                for i, value in enumerate(line, 1):
+                    print(i, value)
+                regel = input("Welke regel wilt u verwijderen? : ")
+                if (regel != "alles"):
+                    regel2= int(regel)
+                    if (isinstance(regel2, int)):
+                        del line[regel2-1]
+                        with open("memes.txt", "w") as c:
+                            c.writelines(line)
+                            print(line)
+                    else:
+                        print("error 345")
+                    waiter = "false"
+                    looper(hoe, waiter)
+                    break
+                elif (regel == "alles"):
+                    with open("memes.txt",  "w") as c:
+                        for i in line :
+                            del line[0]
+                    waiter = "false"
+                    looper(hoe, waiter)
+                    break
+                else:
+                    print("error 543")
+                    waiter = "false"
+                    looper(hoe, waiter)
+                    break
+        elif (hoe == "num"):
+            with open("memes.txt", "r") as reader_num :
+                reader_regel = int(input("Uit welke regel?"))
+                value_1 = reader_num.readlines()
+                value_2 = value_1[reader_regel-1]
+                value_3 = int(value_2)
+                print(value_3)
+                waiter = "false"
+                looper(hoe, waiter)
+                break
+        else:
+            print("?")
+    print("Stopping code")
+    print("~~~~~~~")
+    waiter = "true"
+def begin_code():
+    hoe = ""
+    commandos = ["toev", "read", "del", "num","esc"]
+    while not hoe in commandos:
+        print("~~~~~~~")    
+        print("toev : woord toevoegen")
+        print("read : regel(s) lezen")
+        print("del   : regel verwijderen")
+        print("num : nummer")
+        print("esc  : Programma stoppen")
+        hoe = input("iets : ")
+        waiter = "true"
+        opening = "false"
+        print("~~~~~~~")
+        if (hoe in commandos):
+            gevoel(hoe, waiter)
+
+def looper(hoe, waiter):
+    while (hoe != "esc"):
+        if (waiter == "false"):
+            begin_code()
+        else:
+            print("waiter is dead")
+
+if (opening == "true"):
+    begin_code()
+    opening = "false"
+else:
+    print("ended")
