ee7ea6371bb670c9537472dfdfafd2e6c7fd3881
Quiten <quinten.de.leeuw@coderclass.nl>
2019-10-04 11:32:23 +0200

    dwa
    aertop

--- python_end.py
+++ python_end.py
@@ -1,63 +1,26 @@
 i = 1
-opening = "true"
+opening = True
+funct = ""
 
-def number_checker():
-    a = open("memes.txt", "r")
-    lines = a.readlines()
-    print(lines)
-    a.close()
+def functies_end():    
+    print("Stopping code")
+    print("~~~~~~~")
+    waiter = True
+    funct = "esc"
 
+def functies_num():
+    with open("memes.txt", "r") as reader_num :
+        reader_regel = int(input("Uit welke regel?"))
+        value_1 = reader_num.readlines()
+        value_2 = value_1[reader_regel-1]
+        if (isinstance(int(value_2),int)):
+            print(value_2)
+        else :
+                print("was not an number")
+        waiter = False
+        loop(funct, waiter)
 
-def nummer_erbij(getal):
-    return getal + 1
-
-def gevoel(hoe, waiter):
-    nummer = 1
-    while(hoe != "esc"):
-        if (hoe == "toev"):
-            keuze = input("freestyle of locked ")
-            if (keuze == "locked"):
-                word = input("Woord ")
-                nummero = input("Nummer ")
-                with open("memes.txt", "a") as a:
-                    a.write("Woord : " + word + " , Nummer : " + nummero + "\n")
-                waiter = "false"
-                looper(hoe, waiter)
-            elif (keuze == "freestyle"):
-                freestyle_text = input("Wat moet er in de .txt bestand ")
-                print(freestyle_text)
-                with open("memes.txt", "a") as a:
-                    a.write(freestyle_text + "\n")
-                waiter = "false"
-                looper(hoe, waiter)
-            else:
-                print("error_writer")
-                waiter = "false"
-                looper(hoe, waiter)
-
-
-        elif (hoe == "read"):
-            with open("memes.txt", "r") as b:
-                line = b.readlines()
-                regel = input("Welke regel ? ; ")
-                if (regel != "alles"):
-                    regel2= int(regel)
-                    if (isinstance(regel2, int)):
-                        print(line[regel2-1])
-                    else:
-                        print("Failure")
-                    waiter = "false"
-                    looper(hoe, waiter)
-                elif (regel == "alles"):
-                    for i, value in enumerate(line,1):
-                        print(i, value)
-                    waiter = "false"
-                    looper(hoe, waiter)
-                else:
-                    print("dwadwaa")
-                    waiter = "false"
-                    looper(hoe, waiter)
-        elif (hoe == "del"):
+def functies_del():
             with open("memes.txt","r") as b:
                 line = b.readlines()
             with open("memes.txt", "a") as b:
@@ -73,60 +36,104 @@ def gevoel(hoe, waiter):
                             print(line)
                     else:
                         print("error 345")
-                    waiter = "false"
-                    looper(hoe, waiter)
+                    waiter = False
+                    loop(funct, waiter)
                 elif (regel == "alles"):
                     with open("memes.txt",  "w") as c:
                         for i in line :
                             del line[0]
-                    waiter = "false"
-                    looper(hoe, waiter)
+                    waiter = False
+                    loop(funct, waiter)
                 else:
                     print("error 543")
-                    waiter = "false"
-                    looper(hoe, waiter)
-        elif (hoe == "num"):
-            with open("memes.txt", "r") as reader_num :
-                reader_regel = int(input("Uit welke regel?"))
-                value_1 = reader_num.readlines()
-                value_2 = value_1[reader_regel-1]
-                value_3 = int(value_2)
-                print(value_3)
-                waiter = "false"
-                looper(hoe, waiter)
-        else:
-            print("?")
-    print("Stopping code")
-    print("~~~~~~~")
-    waiter = "true"
-    hoe = "done"
-    looper(hoe,waiter)
+                    waiter = False
+                    loop(funct, waiter)
+
+def functies_toev():
+            keuze = input("freestyle of locked ")
+            if (keuze == "locked"):
+                word = input("Woord ")
+                nummero = input("Nummer ")
+                with open("memes.txt", "a") as a:
+                    a.write("Woord : " + word + " , Nummer : " + nummero + "\n")
+                waiter = False
+                loop(funct, waiter)
+            elif (keuze == "freestyle"):
+                freestyle_text = input("Wat moet er in de .txt bestand ")
+                print(freestyle_text)
+                with open("memes.txt", "a") as a:
+                    a.write(freestyle_text + "\n")
+                waiter = False
+                loop(funct, waiter)
+            else:
+                print("error_writer")
+                waiter = False
+                loop(funct, waiter)
+
+def functies_read():
+            with open("memes.txt", "r") as b:
+                line = b.readlines()
+                regel = input("Welke regel ? ; ")
+                if (regel != "alles"):
+                    regel2= int(regel)
+                    if (isinstance(regel2, int)):
+                        print(line[regel2-1])
+                    else:
+                        print("Failure")
+                    waiter = False
+                    loop(funct, waiter)
+                elif (regel == "alles"):
+                    for i, value in enumerate(line,1):
+                        print(i, value)
+                    waiter = False
+                    loop(funct, waiter)
+                else:
+                    print("false")
+                    waiter = False
+                    loop(funct, waiter)
+
+def functies(funct, waiter):
+    
+    nummer = 1
+    while(funct != "esc"):
+        if (funct == "toev"):
+            functies_toev()
+
+        elif (funct == "read"):
+            functies_read()
+        elif (funct == "del"):
+            functies_del()
+        elif (funct == "num"):
+            functies_num()
+    else :
+        functies_end()
+
 def begin_code():
-    hoe = ""
+    funct = ""
     commandos = ["toev", "read", "del", "num","esc"]
-    while not hoe in commandos:
-        print("~~~~~~~")
+    while not funct in commandos:
+        print("~~~~~~~")    
         print("toev : woord toevoegen")
         print("read : regel(s) lezen")
         print("del   : regel verwijderen")
         print("num : nummer")
         print("esc  : Programma stoppen")
-        hoe = input("iets : ")
-        waiter = "true"
-        opening = "false"
+        funct = input("iets : ")
+        waiter = True
+        opening = False
         print("~~~~~~~")
-        if (hoe in commandos):
-            gevoel(hoe, waiter)
+        if (funct in commandos):
+            functies(funct, waiter)
 
-def looper(hoe, waiter):
-    while (hoe != "esc"):
-        if (waiter == "false"):
+def loop(funct, waiter):
+    while (funct != "esc"):
+        if (waiter == False):
             begin_code()
         else:
             print("stopped")
 
-if (opening == "true"):
-    opening = "false"
+if (opening == True):
+    opening = False
     begin_code()
 else:
     print("ended")
